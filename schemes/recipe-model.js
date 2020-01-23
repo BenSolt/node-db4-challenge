7f070b7d914bfbf293664ff8dc004252d10e4bc3
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-23 17:51:08 -0500

    file bases filled out

---
+++ schemes/recipe-model.js
@@ -0,0 +1,15 @@
+const db = require("../data/db-config")
+
+module.exports = {
+
+};
+
+//GET (FIND)
+
+//GET (FIND BY ID)
+
+//POST (CREATE)
+
+////EDIT/UPDATE - THINK ITS WRONG!
+
+//Delete - THINK ITS WRONG!
